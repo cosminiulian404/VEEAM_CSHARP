9013d3e473754843d305c9854f0270a28df61735
Cosmin <lociccosminiulian@gmail.com>
2023-08-16 21:48:33 +0300

    Modifications and updates.

---
+++ VEEAM1/AppBuilder.cs
@@ -0,0 +1,84 @@
+﻿using System.Timers;
+
+namespace VEEAM1
+{
+    internal class AppBuilder
+    {
+        private static string? _sourceDirectory;
+        private static string? _targetDirectory;
+        private System.Timers.Timer _aTimer;
+
+        public AppBuilder(string sourceDirectory, string targetDirectory, int time)
+        {
+            _aTimer = new System.Timers.Timer(time);
+            _sourceDirectory = sourceDirectory;
+            _targetDirectory = targetDirectory;
+        }
+
+        public void Start()
+        {
+            SetTimer();
+        }
+
+        private void SetTimer()
+        {
+
+            _aTimer.Elapsed += OnTimeEvent;
+
+            _aTimer.AutoReset = true;
+            _aTimer.Enabled = true;
+            _aTimer.Start();
+            Console.ReadLine();
+
+        }
+
+        private void OnTimeEvent(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"Time:{DateTime.Now}.");
+            CopyDir copyDir = new CopyDir(_sourceDirectory!, _targetDirectory!);
+            CompareFiles(_sourceDirectory, _targetDirectory);
+
+
+        }
+
+        private void CompareFiles(string source, string target)
+        {
+            DirectoryInfo checkSource = new DirectoryInfo(source);
+            DirectoryInfo checkTarget = new DirectoryInfo(target);
+
+            foreach (FileInfo sourceFile in checkSource.GetFiles())
+            {
+                foreach (FileInfo targetFile in checkTarget.GetFiles())
+                {
+                    if (sourceFile.Exists && string.Equals(sourceFile.Name, targetFile.Name))
+                    {
+                        Console.WriteLine("XXXXXXXXXXXXXXXXXXXX");
+                        Console.WriteLine($"Copying {sourceFile.FullName}");
+                    }
+                    else if (targetFile.Exists && File.Exists(Path.Combine(targetFile.Name, checkSource.FullName)))
+                    {
+                        Console.WriteLine("////////////");
+                        Console.WriteLine($"Removed {targetFile.Name}");
+                        targetFile.Delete();
+                    }
+                    else if (sourceFile.Exists && File.Exists(Path.Combine(sourceFile.Name, checkTarget.FullName)))
+                    {
+                        Console.WriteLine("+++++++++++++++++++++++");
+                        Console.WriteLine($"Created {sourceFile.FullName}");
+                    }
+
+
+                }
+            }
+            foreach (DirectoryInfo subDir in checkSource.GetDirectories())
+            {
+                foreach (DirectoryInfo subDirTarget in checkTarget.GetDirectories())
+                {
+                    CompareFiles(subDir.FullName, subDirTarget.FullName);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ VEEAM1/CopyDir.cs
@@ -0,0 +1,77 @@
+﻿namespace VEEAM1
+{
+    public class CopyDir
+    {
+        private static DirectoryInfo? _source;
+        private static DirectoryInfo? _target;
+        public static List<string> _coppyedFiles = new List<string>();
+        public static List<string> _removedFiles = new List<string>();
+
+        public CopyDir(string source, string target)
+        {
+            _source = new DirectoryInfo(source);
+            _target = new DirectoryInfo(target);
+            CopyAll(_source, _target);
+
+        }
+
+        public List<string> CoppyedFiles
+        { get { return _coppyedFiles; } }
+
+
+        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+
+            if (source.FullName.ToLower() == target.FullName.ToLower())
+            {
+                return;
+            }
+
+
+           
+
+            // Check if the target directory exists, if not, create it.
+            if (Directory.Exists(target.FullName) == false)
+            {
+                Directory.CreateDirectory(target.FullName);
+            }
+
+            // Copy each file into it's new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+
+                _coppyedFiles.Add(target.FullName + "\\" + fi.Name);
+
+                fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+            }
+
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+
+        }
+
+        public void CompareFiles()
+        {
+           
+
+            foreach (FileInfo sourceFi in _source.GetFiles())
+            {
+                foreach (FileInfo targetFi in _target.GetFiles())
+                {
+                    if (sourceFi == targetFi)
+                        Console.WriteLine($"Copying {sourceFi}");
+
+                }
+            }
+
+        }
+
+
+    }
+}
